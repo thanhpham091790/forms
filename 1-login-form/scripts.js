22de70fb658fbf4b97fdd7cf921edfb5d680fe55
Thanh Pham <thanhpham091790@gmail.com>
2024-06-24 06:54:53 -0600

    Login form

---
+++ 1-login-form/scripts.js
@@ -0,0 +1,8 @@
+document.querySelector("#trigger-btn").addEventListener("click", function () {
+    document.querySelector(".modal").style.display = "block";
+});
+
+document.querySelector("#close-btn").addEventListener("click", function () {
+    document.querySelector(".modal").style.display = "none";
+});
+
